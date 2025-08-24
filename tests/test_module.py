f41559eccb518341e26b85fcd99d0fd330cd7192
zigomon <ken.sony.xperia.0103@gmail.com>
2025-08-24 13:08:29 +0900

    モジュールのインポートパスを修正

--- tests/test_module.py
+++ tests/test_module.py
@@ -1,9 +1,7 @@
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
-
-
-from src.module import foo
+from module import foo
 
 def test_foo_1():
     assert foo(1, 2) == 3
