c35ed64cbbc8637e596aa3ba8352bb198c7b5371
zigomon <ken.sony.xperia.0103@gmail.com>
2025-08-24 11:49:59 +0900

    start code

---
+++ src/module.py
@@ -0,0 +1,11 @@
+def foo(x:int, y:int) -> int:
+    """Adds two integers.
+
+    Args:
+        x (int): The first integer to add.
+        y (int): The second integer to add.
+
+    Returns:
+        int: The sum of the two integers.
+    """
+    return x + y
